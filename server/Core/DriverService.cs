8f1828cc767c24b8926a75b65a488538d5915a9b
Timo Hirvonen <timo.hirvonen@f-secure.com>
2020-02-24 16:19:51 +0200

    Server-side support for automatically unloading the driver

--- server/Core/DriverService.cs
+++ server/Core/DriverService.cs
@@ -41,14 +41,18 @@ namespace Physmem2profit
         protected static extern bool CloseServiceHandle(IntPtr hSCObject);
 
         [DllImport("kernel32", ExactSpelling = true, SetLastError = true, CharSet = CharSet.Auto)]
-        protected static extern bool DeviceIoControl(IntPtr hDevice, uint dwIoControlCode, ref uint lpInBuffer, uint nInBufferSize, byte[] lpOutBuffer, uint nOutBufferSize,
-            out uint lpBytesReturned, IntPtr lpOverlapped);
+        protected static extern bool DeviceIoControl(IntPtr hDevice, uint dwIoControlCode, byte[] inBuffer, uint nInBufferSize, byte[] lpOutBuffer, uint nOutBufferSize,
+            ref uint lpBytesReturned, IntPtr lpOverlapped);
 
         [DllImport("kernel32", SetLastError = true)]
         protected static extern bool SetFilePointerEx(IntPtr hFile, long liDistanceToMove, IntPtr lpNewFilePointer, uint dwMoveMethod);
 
         [DllImport("kernel32", SetLastError = true)]
         protected static extern bool ReadFile(IntPtr hFile, [Out] byte[] lpBuffer, uint nNumberOfBytesToRead, out uint lpNumberOfBytesRead, IntPtr lpOverlapped);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        protected static extern bool CloseHandle(IntPtr hObject);
 
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
         protected struct SERVICE_STATUS_PROCESS
@@ -153,6 +157,7 @@ namespace Physmem2profit
 
         #region Member fields
         private ServiceHandle _handle = new ServiceHandle();
+        private ServiceHandle _hScManager = new ServiceHandle();
         private string _serviceName;
         #endregion Member fields
 
@@ -176,12 +181,12 @@ namespace Physmem2profit
             }
 
             // Grab a handle to the service manager.
-            var hScManager = new ServiceHandle(OpenSCManager(null, null, (uint)AccessRights.SC_MANAGER_ALL_ACCESS));
-            if (!hScManager.IsSet)
+            _hScManager = new ServiceHandle(OpenSCManager(null, null, (uint)AccessRights.SC_MANAGER_ALL_ACCESS));
+            if (!_hScManager.IsSet)
                 ThrowWin32Exception("Failed to get handle to SC Manager.");
 
             // Check if service already exists.
-            _handle = new ServiceHandle(OpenService(hScManager.Handle, _serviceName, (uint)AccessRights.SERVICE_ALL_ACCESS));
+            _handle = new ServiceHandle(OpenService(_hScManager.Handle, _serviceName, (uint)AccessRights.SERVICE_ALL_ACCESS));
             if (_handle.IsSet)
             {
                 Program.Log("Service " + _serviceName + " already exists.", Program.LogMessageSeverity.Warning);
@@ -193,7 +198,7 @@ namespace Physmem2profit
             if (!File.Exists(pathToDriver))
                 throw new Exception("Driver file does not exist.");
 
-            _handle = new ServiceHandle(CreateService(hScManager.Handle, _serviceName, _serviceName, (uint) AccessRights.SERVICE_ALL_ACCESS, (uint) AccessRights.SERVICE_KERNEL_DRIVER,
+            _handle = new ServiceHandle(CreateService(_hScManager.Handle, _serviceName, _serviceName, (uint) AccessRights.SERVICE_ALL_ACCESS, (uint) AccessRights.SERVICE_KERNEL_DRIVER,
                 (uint) AccessRights.SERVICE_DEMAND_START, (uint) AccessRights.SERVICE_ERROR_IGNORE, Path.GetFullPath(pathToDriver), null, null, null, null, null));
             if (!_handle.IsSet)
                 ThrowWin32Exception("Failed to create service.");
@@ -208,6 +213,7 @@ namespace Physmem2profit
                 ThrowWin32Exception("Failed to delete service.");
 
             _handle.Close();
+            _hScManager.Close();
         }
 
         /// <summary>
